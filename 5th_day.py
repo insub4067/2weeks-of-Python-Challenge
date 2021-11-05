c1c702793a33b8360c37732317accfc204fba01f
insub kim <85481204+insub4067@users.noreply.github.com>
2021-11-06 03:00:28 +0900

    Add files via upload

---
+++ 5th_day.py
@@ -0,0 +1,58 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+os.system("clear")
+url = "https://www.iban.com/currency-codes"
+
+iban_result = requests.get(url)
+
+iban_soup = BeautifulSoup(iban_result.text, 'html.parser')
+
+table = iban_soup.table
+
+trs = table.find_all('tr')
+
+print("Hello! Please choose select a country by number: ")
+
+currency_list=[]
+
+def index(country_number):
+    try:
+        res = currency_list[country_number-1]
+        country = res['country']
+        currency = res['currency']
+        print(f"You chose {country}")
+        print(f"The currency code is {currency}")
+    except:
+        print("Choose a nuber from the list.")
+        req()
+
+def req():
+    try:
+        country_number = int(input("#: "))
+        index(country_number)
+    except:
+        print("That's not a number.")
+        req()
+
+
+
+for idx, tr in enumerate(trs): 
+    if idx > 0:
+        tds = tr.find_all('td')
+        country = tds[0].text.strip()
+        country = str(country).capitalize()
+        currency = tds[1].text.strip()
+        code = tds[2].text.strip()
+        number = tds[3].text.strip()
+
+        currency_list.append({
+            "idx":idx, 
+            "country": country,
+            "currency": currency,
+            })
+
+        print("#", idx,country)
+
+req()
